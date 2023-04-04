0291e43151690311a59a0ee73e56dfb4977826e0
Doğukan KÖR <56999173+dogukan-kor@users.noreply.github.com>
2023-04-05 00:20:31 +0300

    paket eklemeleri yapıldı

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register= async (req,res,next)=>{
     try{
@@ -20,15 +21,23 @@ export const register= async (req,res,next)=>{
     }
 }
 
-export const  login= async (req,res,next)=>{
+export const login= async (req,res,next)=>{
     try{
          const user = await User.findOne({username:req.body.username})
          if(!user) return next(createError(404,"Kullanıcı Bulunamadı!"))
          
          const isPasswordCorrect=await bcrypt.compare(req.body.password,user.password)
          if(!isPasswordCorrect) return next(createError(400,"Şifre Hatalı"))
-         
-         res.status(200).json(user)
+
+         const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.JWT);
+ 
+         const{password,isAdmin,...otherDetails}=user._doc //parantez içinde" olanlar gizli
+         res
+         .cookie("access_token",token,{
+            httpOnly:true,
+         })
+         .status(200)
+         .json(otherDetails);
 
     }catch(err){
         next(err)
